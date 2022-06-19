d8c9a9fcfeab9b20320faf4acb74586a8fd227f1
zaba505 <cakub6@gmx.com>
2022-06-18 20:55:46 -0400

    feat(cmd/cp): implement copying to and from hfs

--- http/client.go
+++ http/client.go
@@ -1,9 +1,11 @@
 package http
 
 import (
+	"bytes"
 	"context"
 	"errors"
 	"fmt"
+	"io"
 	"net/http"
 )
 
@@ -19,6 +21,60 @@ func NewClient() *FileServerClient {
 	return &FileServerClient{
 		httpClient: http.DefaultClient,
 	}
+}
+
+type DownloadRequest struct {
+	Path string
+}
+
+type DownloadResponse struct {
+	Body io.Reader
+}
+
+func (c *FileServerClient) Download(ctx context.Context, req *DownloadRequest) (*DownloadResponse, error) {
+	r, err := http.NewRequestWithContext(ctx, http.MethodGet, req.Path, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	resp, err := c.httpClient.Do(r)
+	if err != nil {
+		return nil, err
+	}
+	if resp.StatusCode == http.StatusNotFound {
+		return nil, ErrFileNotFound
+	}
+	if resp.StatusCode != http.StatusOK {
+		return nil, fmt.Errorf("http: unexpected http status code received - %d", resp.StatusCode)
+	}
+
+	var buf bytes.Buffer
+	_, err = copy(&buf, resp.Body)
+	if err != nil {
+		return nil, err
+	}
+	return &DownloadResponse{Body: &buf}, nil
+}
+
+type UploadRequest struct {
+	Path    string
+	Content io.Reader
+}
+
+func (c *FileServerClient) Upload(ctx context.Context, req *UploadRequest) error {
+	r, err := http.NewRequestWithContext(ctx, http.MethodPost, req.Path, req.Content)
+	if err != nil {
+		return err
+	}
+
+	resp, err := c.httpClient.Do(r)
+	if err != nil {
+		return err
+	}
+	if resp.StatusCode != http.StatusOK && resp.StatusCode != http.StatusCreated {
+		return fmt.Errorf("http: unexpected http status code received - %d", resp.StatusCode)
+	}
+	return nil
 }
 
 // RemoveRequest
